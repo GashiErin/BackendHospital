7fc05be407a16a3d66a05f9f574b0dfe11e41a01
Drenniii <149413647+Drenniii@users.noreply.github.com>
2025-05-28 14:59:13 +0200

    Modify UserController and UserService

---
+++ spring-boot-3-jwt-security-main/src/main/java/com/example/Hospital/security/appointment/Appointment.java
@@ -0,0 +1,46 @@
+package com.example.Hospital.security.appointment;
+
+import com.example.Hospital.security.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "appointments")
+public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "client_id", nullable = false)
+    private User client;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "professional_id", nullable = false)
+    private User professional;
+
+    @Column(nullable = false)
+    private LocalDateTime appointmentDateTime;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AppointmentType type;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AppointmentStatus status;
+
+    private String notes;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+}
